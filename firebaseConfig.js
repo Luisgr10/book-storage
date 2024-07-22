218c2d3b0ec740b25f7eb58d270629f4375bc32f
Luisgr <luijo915@hotmail.com>
2024-07-22 20:21:15 +0100

    initial commit

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,32 +1,38 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Platform } from 'react-native';
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Platform } from "react-native";
+
 
 // Configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyALWO6YaYcefKul_Y8Wh4pt2loHvlXEAQg",
-  authDomain: "pt-kindle-clone.firebaseapp.com",
-  projectId: "pt-kindle-clone",
-  storageBucket: "pt-kindle-clone.appspot.com",
-  messagingSenderId: "614967430643",
-  appId: "1:614967430643:web:4ad24c2788ab0596071686"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
+
 
 // Inicialización de Firebase
 const appFirebase = initializeApp(firebaseConfig);
 
 // Inicialización de Auth
 let auth;
-if (Platform.OS === 'web') {
+if (Platform.OS === "web") {
   // Web
   auth = getAuth(appFirebase);
 } else {
   // Mobile
   auth = initializeAuth(appFirebase, {
-    persistence: getReactNativePersistence(AsyncStorage)
+    persistence: getReactNativePersistence(AsyncStorage),
   });
 }
 
